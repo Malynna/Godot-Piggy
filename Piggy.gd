fbe6f1304c8e5110ba80683ec5239c3bb25ddb9c
Malynna <malinex@op.pl>
2022-03-27 19:46:58 +0200

    Added export int SPEED

--- Piggy.gd
+++ Piggy.gd
@@ -1,11 +1,16 @@
 extends Area2D
 
+export(int) var SPEED = 100
+
 func _process(delta):
 	if Input.is_action_pressed("ui_right"):
-		position.x += 2
+		move (SPEED,0, delta)
 	if Input.is_action_pressed("ui_left"):
-		position.x -= 2
+		move (-SPEED,0, delta)
 	if Input.is_action_pressed("ui_up"):
-		position.y -= 2
+		move (0,-SPEED, delta)
 	if Input.is_action_pressed("ui_down"):
-		position.y += 2
+		move (0,SPEED, delta)
+func move(xspeed, yspeed, delta):
+	position.x += xspeed * delta
+	position.y += yspeed * delta
